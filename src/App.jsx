299ec31cf03db5a2aac09b2be8577014d7a6cfa1
Mohitbhatt27 <mohit.peak@gmail.com>
2024-04-19 01:28:02 +0530

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import "./App.css";
+import Login from "./pages/auth/Login";
 
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline text-center">
-          Hello world! 
-          <button className="btn btn-primary">Primary</button>
-          <button className="btn btn-secondary">Secondary</button>
-          <button className="btn btn-accent">Accent</button>
-        </h1>
-      </div>
+      <Login />
     </>
   );
 }
