9530a28a0d760e015647d8ba7e66cbd1d3ba13fa
Mohitbhatt27 <mohit.peak@gmail.com>
2024-04-18 18:42:36 +0530

    integrated daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 
 function App() {
-  
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-    </h1>
+      <div>
+        <h1 className="text-3xl font-bold underline text-center">
+          Hello world! 
+          <button className="btn btn-primary">Primary</button>
+          <button className="btn btn-secondary">Secondary</button>
+          <button className="btn btn-accent">Accent</button>
+        </h1>
+      </div>
     </>
   );
 }
