42f58f09b5e57fd743aed5ea62e49f8cf2627cb5
Mohitbhatt27 <mohit.peak@gmail.com>
2024-04-20 20:37:24 +0530

    Added react hot toast

--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
 
 import axiosInstance from "../../config/axiosInstance";
 
@@ -11,23 +12,31 @@ const initialState = {
 
 export const login = createAsyncThunk("/auth/login", async (data) => {
   try {
-    const response = await axiosInstance.post("auth/signin", data);
-    return response;
+    const response = axiosInstance.post("auth/signin", data);
+    toast.promise(response, {
+      loading: "Submitting form",
+      success: "Successfully signed in",
+      error: "Something went wrong, try again",
+    });
+    return await response;
   } catch (error) {
     console.log(error);
   }
 });
 
-export const signup = createAsyncThunk("auth/signup", async(data)=>{
+export const signup = createAsyncThunk("auth/signup", async (data) => {
   try {
-    const response = await axiosInstance.post("auth/signup", data);
-    return response;
-    
+    const response = axiosInstance.post("auth/signup", data);
+    toast.promise(response, {
+      loading: "Submitting form",
+      success: "Successfully signed up",
+      error: "Something went wrong, try again",
+    });
+    return await response;
   } catch (error) {
     console.log(error);
-    
   }
-})
+});
 
 const authSlice = createSlice({
   name: "auth",
