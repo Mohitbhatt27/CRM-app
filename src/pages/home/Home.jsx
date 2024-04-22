ca6bcf177b2f5af3370340d0e58d12c583296a52
Mohitbhatt27 <mohit.peak@gmail.com>
2024-04-23 00:16:50 +0530

    Added line chart with multiple lines

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -28,10 +28,13 @@ ChartJS.register(
   PointElement,
   CategoryScale
 );
-
 function Home() {
   const [ticketsState] = useTickets();
-  const [openTickets, setOpenTickets] = useState({});
+  const [ticketsChartData, setTicketsChartData] = useState({
+    openTickets: [],
+    inProgressTickets: [],
+    resolvedTickets: [],
+  });
 
   const pieChartData = {
     labels: Object.keys(ticketsState.ticketDistribution),
@@ -48,40 +51,104 @@ function Home() {
   };
 
   const lineChartData = {
-    labels: Object.keys(openTickets),
+    labels: Object.keys(ticketsChartData.openTickets),
     fontColor: "white",
     datasets: [
       {
         label: "Open Tickets data",
-        data: Object.values(openTickets),
+        data: Object.values(ticketsChartData.openTickets),
         borderColor: "rgb(255, 99, 132)",
+      },
+      {
+        label: "In Progress Tickets data",
+        data: Object.values(ticketsChartData.inProgressTickets),
+        borderColor: "rgb(53, 162, 235)",
+      },
+      {
+        label: "Resolved Tickets data",
+        data: Object.values(ticketsChartData.resolvedTickets),
+        borderColor: "rgb(245, 205, 95)",
+        borderWidth: 4,
       },
     ],
   };
 
-  useEffect(() => {
-    console.log(ticketsState);
+  function processTickets() {
+    // Fetch the current Date
+    const currentDate = new Date();
+    // Calculate the 10th date from today
+    const tenthDayFromToday = new Date();
+    tenthDayFromToday.setDate(currentDate.getDate() - 10);
+
+    // Process all the tickets
     if (ticketsState.ticketList.length > 0) {
+      // Prepare two localobjects to act as frequency map
       let openTicketsData = {};
+      let inProgressTicketsData = {};
+      let resolvedTicketsData = {};
+
+      // Initialise the frequency map with default value 0 for the last 10 days
+      for (let i = 0; i < 10; i++) {
+        // Get the ith day from today
+        const dateObject = new Date();
+        dateObject.setDate(currentDate.getDate() - i);
+        /**
+         * dateObject.toLocaleDateString() -> gives us string in the format DD/MM/YYYY
+         * Convert this to YYYY-MM-DD
+         */
+        openTicketsData[
+          dateObject.toLocaleDateString().split("/").reverse().join("-")
+        ] = 0;
+        inProgressTicketsData[
+          dateObject.toLocaleDateString().split("/").reverse().join("-")
+        ] = 0;
+        resolvedTicketsData[
+          dateObject.toLocaleDateString().split("/").reverse().join("-")
+        ] = 0;
+      }
+
+      // Process all the tickets one by one
       ticketsState.ticketList.forEach((ticket) => {
+        // Get the date part from the tickets by removing everything post the character T
         const date = ticket.createdAt.split("T")[0];
-        if (ticket.status == "open") {
-          openTicketsData[date] = !openTicketsData[date]
-            ? 1
-            : openTicketsData[date] + 1;
+        const ticketDate = new Date(ticket.createdAt);
+        // If ticket is open and lies in the last 10 days add it
+        if (ticket.status == "open" && ticketDate >= tenthDayFromToday) {
+          openTicketsData[date] = openTicketsData[date] + 1;
+        }
+
+        // If ticket is inProgress and lies in the last 10 days add it
+        if (ticket.status == "inProgress" && ticketDate >= tenthDayFromToday) {
+          inProgressTicketsData[date] = inProgressTicketsData[date] + 1;
+        }
+
+        // If ticket is resolved and lies in the last 10 days add it
+        if (ticket.status == "resolved" && ticketDate >= tenthDayFromToday) {
+          resolvedTicketsData[date] = resolvedTicketsData[date] + 1;
+        }
+        if (ticket.status == "resolved") {
+          console.log(ticket);
         }
       });
-      setOpenTickets(openTicketsData);
+      //  update the state
+      console.log(resolvedTicketsData);
+      setTicketsChartData({
+        openTickets: openTicketsData,
+        inProgressTickets: inProgressTicketsData,
+        resolvedTickets: resolvedTicketsData,
+      });
     }
+  }
+
+  useEffect(() => {
+    console.log(ticketsState);
+    processTickets();
   }, [ticketsState.ticketList]);
 
   return (
     <HomeLayout>
       {ticketsState && (
-        <div
-          className="mt-10 flex w-4/5 flex-row justify-center items-center gap-x-24 gap-y-6 flex-wrap font-mono ...
-      "
-        >
+        <div className="mt-10 flex flex-row justify-center items-center gap-5 flex-wrap">
           <Card
             titleText="Open"
             status={
@@ -90,7 +157,7 @@ function Home() {
             }
             quantity={ticketsState.ticketDistribution.open}
             background="bg-yellow-300"
-            borderColor="border-[#7E2F81]"
+            borderColor="border-green-300"
             fontColor="text-black"
             dividerColor="bg-black"
           >
@@ -104,7 +171,7 @@ function Home() {
             }
             quantity={ticketsState.ticketDistribution.inProgress}
             background="bg-orange-300"
-            borderColor="border-[#87A4C5]"
+            borderColor="border-red-300"
             fontColor="text-black"
             dividerColor="bg-black"
           >
@@ -118,7 +185,7 @@ function Home() {
             }
             quantity={ticketsState.ticketDistribution.resolved}
             background="bg-purple-300"
-            borderColor="border-[#FFF1BE]"
+            borderColor="border-blue-700"
             fontColor="text-black"
             dividerColor="bg-black"
           >
@@ -132,7 +199,7 @@ function Home() {
             }
             quantity={ticketsState.ticketDistribution.onHold}
             background="bg-gray-300"
-            borderColor="border-[#F4E3D4]"
+            borderColor="border-gray-800"
             fontColor="text-black"
             dividerColor="bg-black"
           >
@@ -146,7 +213,7 @@ function Home() {
             }
             quantity={ticketsState.ticketDistribution.cancelled}
             background="bg-blue-300"
-            borderColor="border-[#FFA77D]"
+            borderColor="border-violet-300"
             fontColor="text-black"
             dividerColor="bg-black"
           >
@@ -154,16 +221,14 @@ function Home() {
           </Card>
         </div>
       )}
-      <div>
-        <div className="mt-10 flex justify-center items-center gap-10">
-          <div className="w-80 h-80 ">
-            <Pie data={pieChartData} />
-          </div>
+      <div className="mt-10 flex justify-center items-center gap-10">
+        <div className="w-80 h-80 ">
+          <Pie data={pieChartData} />
         </div>
-        <div className="mt-10 flex justify-center items-center gap-10">
-          <div className="w-[40rem] h-[40rem]">
-            <Line data={lineChartData} />
-          </div>
+      </div>
+      <div className="mt-10 mb-10 flex justify-center items-center gap-10">
+        <div className="w-[50rem] bg-[wheat]">
+          <Line data={lineChartData} />
         </div>
       </div>
     </HomeLayout>
